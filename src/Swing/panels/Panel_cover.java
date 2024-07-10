d4d7148a695bfaf14169db6ebc340d3b9f0f00a9
noface-00 <morakevin269@gmail.com>
2024-07-10 01:17:53 -0500

    Panel con logo y fondo degradado

--- src/Swing/panels/Panel_cover.java
+++ src/Swing/panels/Panel_cover.java
@@ -9,6 +9,9 @@ import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.net.URL;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import net.miginfocom.swing.MigLayout;
@@ -20,61 +23,43 @@ import swing.controls.ButtonOutLine;
  * @author PC
  */
 public class Panel_cover extends javax.swing.JPanel {
+    private final DecimalFormat df = new DecimalFormat("##0.###", DecimalFormatSymbols.getInstance(Locale.US));
     private ActionListener event;
     private MigLayout layout;
     private JLabel Logo;
+    private JLabel eslogan_1;
     private ButtonOutLine button;
     private Image backgroundImage;
+    private boolean isLogin;
     
     public Panel_cover() {
         initComponents();
-        init();
+        setOpaque(false);
         layout = new MigLayout("wrap, fill", "[center]","push[]25[200]10[]25[]push");
         setLayout(layout);
-        setOpaque(false);
+        init(); 
         
     }
     private void init() {
         Logo = new JLabel();
-                // Cargar la imagen de fondo
-        try {
-            URL bgUrl = getClass().getResource("/icons/readventure/Stickers.png");
-            if (bgUrl != null) {
-                backgroundImage = new ImageIcon(bgUrl).getImage();
-            } else {
-                System.out.println("Imagen de fondo no encontrada.");
-            }
-        } catch (Exception e) {
-            System.out.println("Error al cargar la imagen de fondo: " + e.getMessage());
-        }
-        // Verificación manual del recurso
-        try {
-            URL resourceUrl = getClass().getResource("/icons/readventure/logo.png");
-            if (resourceUrl != null) {
-                System.out.println("Recurso encontrado: " + resourceUrl.toString());
-                ImageIcon icono = new ImageIcon(resourceUrl);
-                if (icono.getImageLoadStatus() == java.awt.MediaTracker.COMPLETE) {
-                    Logo.setIcon(icono);
-                } else {
-                    System.out.println("No se pudo cargar la imagen. Estado de carga: " + icono.getImageLoadStatus());
-                }
-            } else {
-                System.out.println("Recurso no encontrado.");
-            }
-        } catch (Exception e) {
-            System.out.println("Error al cargar la imagen: " + e.getMessage());
-        }
+        backgroundImage = new ImageIcon(getClass().getResource("/icons/readventure/Stickers.png")).getImage();
+        ImageIcon icono = new ImageIcon(getClass().getResource("/icons/readventure/logo.png"));
+        Logo.setIcon(icono);
         add(Logo);
         
+        eslogan_1 = new JLabel("¡Regístrate y descubre un mundo de historias por leer!");
+        eslogan_1.setFont(new Font("Roboto Black", 1, 23));
+        eslogan_1.setForeground(new Color(247,198,57));
+        add(eslogan_1);
         button = new ButtonOutLine();
         button.setBackground(new Color(255,255,255));
         button.setForeground(new Color(255,255,255));
-        button.setText("Registrate");
-        button.setFont(new Font("Roboto", 1, 35));
+        button.setText("Inicia Sesion");
+        button.setFont(new Font("Roboto Black", 1, 20));
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-               event.actionPerformed(e);
+                    event.actionPerformed(e);
             }
         });
         add(button, "width 200px!, height 40px!");
@@ -113,6 +98,48 @@ public class Panel_cover extends javax.swing.JPanel {
 
     public void addEvent(ActionListener event) {
         this.event = event;
+    }
+    
+    public void registerLeft(double v) {
+        v = Double.valueOf(df.format(v));
+        login(false);
+        layout.setComponentConstraints(Logo, "pad 0 -" + v + "% 0 0");
+        layout.setComponentConstraints(eslogan_1, "pad 0 -" + v + "% 0 0");
+        //layout.setComponentConstraints(description1, "pad 0 -" + v + "% 0 0");
+    }
+
+    public void registerRight(double v) {
+        v = Double.valueOf(df.format(v));
+        login(false);
+        layout.setComponentConstraints(Logo, "pad 0 -" + v + "% 0 0");
+        layout.setComponentConstraints(eslogan_1, "pad 0 -" + v + "% 0 0");
+        //layout.setComponentConstraints(description1, "pad 0 -" + v + "% 0 0");
+    }
+
+    public void loginLeft(double v) {
+        v = Double.valueOf(df.format(v));
+        login(true);
+        layout.setComponentConstraints(Logo, "pad 0 " + v + "% 0 " + v + "%");
+        layout.setComponentConstraints(eslogan_1, "pad 0 " + v + "% 0 " + v + "%");
+        //layout.setComponentConstraints(description1, "pad 0 " + v + "% 0 " + v + "%");
+    }
+
+    public void loginRight(double v) {
+        v = Double.valueOf(df.format(v));
+        login(true);
+        layout.setComponentConstraints(Logo, "pad 0 " + v + "% 0 " + v + "%");
+        layout.setComponentConstraints(eslogan_1, "pad 0 " + v + "% 0 " + v + "%");
+        //layout.setComponentConstraints(description1, "pad 0 " + v + "% 0 " + v + "%");
+    }
+    public void login(boolean login){
+        if(this.isLogin != login){
+            eslogan_1.setText("¡Inicia sesión y continúa tu aventura literaria!");
+            button.setText("Registrate");
+        }else{
+            eslogan_1.setText("¡Regístrate y descubre un mundo de historias por leer!");
+            button.setText("Inicia Sesion");
+        }
+        //this.isLogin = login;
     }
     // Variables declaration - do not modify//GEN-BEGIN:variables
     // End of variables declaration//GEN-END:variables
